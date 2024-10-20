d45302dbd5cff5e5fda0dba467a03883a5377067
afgodix <maksimkotofeev0@gmail.com>
2024-10-21 01:05:26 +0300

    Update 1.1
    Добавлена функция перевода на испанский язык. Инструкции по переводу содержатся в стартовом сообщении бота.

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-#TranBot                                                                        '''Бот для перевода текста'''
-
 import telebot, deeptranslate as tran
-token = input('insert your token... ')
+
+token = input('insert token: ')
 print('starting...')
 
-bot = telebot.TeleBot(token)                                            #Токен бота
+bot = telebot.TeleBot(token)
 
 print('the bot has started!')
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.send_message(message.from_user.id, 'Привет! 👋 Я - TranBot. Я способен переводить текст с русского языка на английский или наоборот. Для начала работы введите фразу для перевода. ')
+    bot.send_message(message.from_user.id, 'Привет! 👋 Я - TranBot. Я способен переводить текст с русского языка на английский или испанский и наоборот. Для начала работы введите фразу для перевода. Укажите целевой язык (en или es), например: "en: Привет" или "es: Hola".')
 
-@bot.message_handler(content_types=['text'])                                    #Метод для получения текст. сообщений
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    bot.send_message(message.from_user.id, 'Загрузка... ⏳')
-    bot.edit_message_text(tran.translateEn(message.text), message.from_user.id, message.id + 1)
-    print('RuEN - ', message.text, ' -- ', tran.translateEn(message.text))
+    text = message.text.strip()
+    target_language = text.split(':')[0].strip()  # Retrieve the language code
+    text_to_translate = ':'.join(text.split(':')[1:]).strip()  # Retrieve text to translate
 
-bot.infinity_polling(none_stop = True, interval = 0)
+    bot.send_message(message.from_user.id, 'Загрузка... ⏳')
+
+    try:
+        if target_language == 'en':
+            translated_text = tran.translateEn(text_to_translate)
+        elif target_language == 'es':
+            translated_text = tran.translateEs(text_to_translate)
+        else:
+            bot.send_message(message.from_user.id, 'Неправильный язык. Используйте "en:" для английского и "es:" для испанского.')
+            return
+        
+        bot.edit_message_text(translated_text, message.from_user.id, message.id + 1)
+        print(f'Ru{target_language.upper()} - {text_to_translate} -- {translated_text}')
+    except Exception as e:
+        bot.send_message(message.from_user.id, 'Произошла ошибка при переводе. Попробуйте еще раз.')
+
+bot.infinity_polling(none_stop=True, interval=0)
